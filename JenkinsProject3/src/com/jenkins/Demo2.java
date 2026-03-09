88d032383d29e3b5fd61040d50f04e11684c8826
Chayan Koley <chayankoley0741@gmail.com>
2026-03-09 14:45:36 +0530

    Add files via upload

---
+++ JenkinsProject3/src/com/jenkins/Demo2.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a=30;
+		int b=20;
+		
+		System.out.println("You are good to use Jenkins! Smile");
+
+	}
+
+}
